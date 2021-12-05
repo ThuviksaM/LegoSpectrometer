079a2fe53cff180bb86735fd48138fc6e0b830d5
Thuviksa <90987157+ThuviksaM@users.noreply.github.com>
2021-12-05 12:08:15 -0500

    Update and rename index.js to spectra.js

---
+++ docs/diff-languages/spectra.js
@@ -0,0 +1,55 @@
+/*--------------------------------------------------------------------------------------*/
+
+/* Search Bar */
+function filterSpectra() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/* Modal (doesn't work 😭) */
+        // Get the modal
+        var modal = document.getElementById("videoModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("watchBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("closeModal")[0];
+
+        // When the user clicks on the button, open the modal
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+
+/*--------------------------------------------------------------------------------------*/
